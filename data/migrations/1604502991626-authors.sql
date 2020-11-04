829637157ac6d6cd1718649ca0508d07576cb31c
sallytareq <sally.ammous@gmail.com>
2020-11-04 19:30:43 +0200

    lab14

--- data/migrations/1604502991626-authors.sql
+++ data/migrations/1604502991626-authors.sql
@@ -7,14 +7,14 @@
 -- CREATE DATABASE books_app_normal WITH TEMPLATE books_app;
 -- psql -f /data/migrations/1604502991626-authors.sql -d books_app_normal
 
--- DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS authors;
 
--- CREATE TABLE authors (id SERIAL PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE authors (id SERIAL PRIMARY KEY, name VARCHAR(255));
 
--- INSERT INTO authors(name) SELECT DISTINCT author FROM books;
+INSERT INTO authors(name) SELECT DISTINCT author FROM books;
 
--- ALTER TABLE books ADD COLUMN author_id INT;
+ALTER TABLE books ADD COLUMN author_id INT;
 
--- UPDATE books SET author_id=author.id FROM (SELECT * FROM authors) AS author WHERE books.author = author.name;
+UPDATE books SET author_id=author.id FROM (SELECT * FROM authors) AS author WHERE books.author = author.name;
 
--- ALTER TABLE books ADD CONSTRAINT fk_authors FOREIGN KEY (author_id) REFERENCES authors(id);
+ALTER TABLE books ADD CONSTRAINT fk_authors FOREIGN KEY (author_id) REFERENCES authors(id);
